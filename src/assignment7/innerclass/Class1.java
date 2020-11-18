981bd02fa94e2d2a6e664eed1199e77aceb8468d
Santhosh Challa <santhosh.challa@zemosolabs.com>
2020-11-18 18:16:54 +0530

    Asg7 change

--- src/assignment7/innerclass/Class1.java
+++ src/assignment7/innerclass/Class1.java
@@ -1,11 +1,12 @@
 package assignment7.innerclass;
 
 public class Class1 {
-    class inner1{
+    class Inner1{
         String name;
-        inner1(){}
-        public inner1(String name){
+        Inner1(){}
+        public Inner1(String name){
             this.name = name;
+            System.out.println("Inner1: Value of param name is: "+ name);
         }
     }
 }
--- src/assignment7/innerclass/Class2.java
+++ src/assignment7/innerclass/Class2.java
@@ -1,16 +1,15 @@
 package assignment7.innerclass;
 
-public class Class2{
-    class inner2 extends Class1.inner1{
-        /*
-         * Compiler is trowing the error if there is no default constructor
-         * in inner class1.
-         *
-         * Also, need to make a call to inner class constructor  using
-         * Class1, to avoid another error.
-         */
-    inner2(Class1 cls){
-        cls.super("Santhosh");
+public class Class2 {
+    static class Inner2 extends Class1.Inner1 {
+        String name;
+
+        Inner2(String name) {
+            new Class1().super(name);
+        }
     }
+
+    public static void main(String[] args) {
+        Class2.Inner2 in = new Class2.Inner2("santhosh");
     }
 }
